6ee634ff9000de1401a743d17a9e8405cfbdf971
Ankitray420 <81009714+Ankitray420@users.noreply.github.com>
2021-03-20 13:53:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+﻿//responsive topnav--------------------------------------------------------------------------------------------------------------------------
+    function myFunction() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
+//food varites-------------------------------------------------------------------------------------------------------------------------------
+    function varites(vartiesName) {
+      var i;
+      var x = document.getElementsByClassName("varites");
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+      document.getElementById(vartiesName).style.display = "block";
+    }
+    
+//cart---------------------------------------------------------------------------------------------------------------------------------------
