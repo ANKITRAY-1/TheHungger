de6ad76258474a48a94eeb3920906216e7eb799b
Ankitray420 <81009714+Ankitray420@users.noreply.github.com>
2021-03-22 21:28:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿//responsive topnav--------------------------------------------------------------------------------------------------------------------------
+//responsive topnav--------------------------------------------------------------------------------------------------------------------------
     function myFunction() {
       var x = document.getElementById("myTopnav");
       if (x.className === "topnav") {
@@ -7,7 +7,7 @@
         x.className = "topnav";
       }
     }
-//food varites-------------------------------------------------------------------------------------------------------------------------------
+//food varites-------------------------------------------------------------------------------------------------------------------------
     function varites(vartiesName) {
       var i;
       var x = document.getElementsByClassName("varites");
@@ -17,4 +17,30 @@
       document.getElementById(vartiesName).style.display = "block";
     }
     
-//cart---------------------------------------------------------------------------------------------------------------------------------------
+//readmore-----------------------------------------------------------------------------------------------------------------------------
+function readFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+//loading---------------------------------------------------------------------------------------------------------------------------
+var myVar;
+
+function loadFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
